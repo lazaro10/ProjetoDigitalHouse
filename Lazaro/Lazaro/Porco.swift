d702c0a60cc195c20ca81bf458ca4336bb028bbd
Giuliano Accorsi <giulianoaccorsi@MacBook-Pro-de-Giuliano.local>
2019-05-09 21:54:21 -0300

    Cria classe porco

---
+++ Lazaro/Lazaro/Porco.swift
@@ -0,0 +1,13 @@
+//
+//  Porco.swift
+//  Lazaro
+//
+//  Created by Giuliano Accorsi on 09/05/19.
+//  Copyright © 2019 Digital House. All rights reserved.
+//
+
+import Foundation
+
+class Porco {
+    var nome: String = "Pork"
+}
