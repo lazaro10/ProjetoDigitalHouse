f48eaa2f0536aa4dfbc68fb8f27ad1237abb4054
Digital House <digitalhouse@DH-A1-22.local>
2019-05-09 21:28:37 -0300

    Cria classe Pessoa

---
+++ Lazaro/Lazaro/Pessoa.swift
@@ -0,0 +1,14 @@
+//
+//  Pessoa.swift
+//  Lazaro
+//
+//  Created by Digital House on 09/05/19.
+//  Copyright © 2019 Digital House. All rights reserved.
+//
+
+import Foundation
+
+class Pessoa {
+    var nome: String = "Lázaro"
+    var idade: Int = 24
+}
